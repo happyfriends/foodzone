c255800576785803bbaf8b3a41192e3e9f6ee924
cored <george.rafael@gmail.com>
2015-08-24 16:39:00 -0400

    Remove single quotes string for consistency

--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -1,21 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Venue, type: :model do
-  describe '.save_venue' do
-    let(:name) { 'burguer shop' }
+  describe ".save_venue" do
+    let(:name) { "burguer shop" }
 
-    it 'creates a new venue' do
+    it "creates a new venue" do
       expect {
         Venue.save_venue(name)
       }.to change(Venue, :count).by 1
     end
   end
 
-  describe '#add_branch' do
+  describe "#add_branch" do
     let(:venue) { FactoryGirl.create :venue }
     let(:branch_attrs) { FactoryGirl.attributes_for :branch }
 
-    it 'creates a new branch for the current venue' do
+    it "creates a new branch for the current venue" do
       expect {
         venue.add_branch(branch_attrs)
       }.to change { venue.branches.count }.by 1
