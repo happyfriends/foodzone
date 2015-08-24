ca3d23876a3cee4656b01a8389999c6d9adcc8dc
cored <george.rafael@gmail.com>
2015-08-24 16:40:08 -0400

    Avoid using { } for multi line blocks

--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Venue, type: :model do
     let(:name) { "burguer shop" }
 
     it "creates a new venue" do
-      expect {
+      expect do
         Venue.save_venue(name)
-      }.to change(Venue, :count).by 1
+      end.to change(Venue, :count).by 1
     end
   end
 
@@ -16,9 +16,9 @@ RSpec.describe Venue, type: :model do
     let(:branch_attrs) { FactoryGirl.attributes_for :branch }
 
     it "creates a new branch for the current venue" do
-      expect {
+      expect do
         venue.add_branch(branch_attrs)
-      }.to change { venue.branches.count }.by 1
+      end.to change { venue.branches.count }.by 1
     end
   end
 end
